2a65121d7bbd60510c622db6ddf6a94019a6515a
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2022-01-21 02:24:23 -0300

    Fixed jsonAPI

--- js/node/jsonAPI.js
+++ js/node/jsonAPI.js
@@ -5,12 +5,12 @@ const salt = "ThisIsASimpleSalt";
 
 module.exports = {
     getClient: function (client) {
-        return axios.get(baseUrl+"/client?username="+client.username);
+        return axios.get(baseUrl+"/cliente?username="+client.username);
     },
     getClients: function () {
-        return axios.get(baseUrl+"/client");
+        return axios.get(baseUrl+"/cliente");
     },
     addClient: function (client) {
-        return axios.post(baseUrl+"/client",client);
+        return axios.post(baseUrl+"/cliente",client);
     }
 }
